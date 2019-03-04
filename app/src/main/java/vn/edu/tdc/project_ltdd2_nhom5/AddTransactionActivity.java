43994dfba7d590ddba2b203d77df22af01f7d238
Phu Vo <vongocphu04@gmail.com>
2019-03-04 11:09:13 +0700

    phu 2

---
+++ app/src/main/java/vn/edu/tdc/project_ltdd2_nhom5/AddTransactionActivity.java
@@ -0,0 +1,13 @@
+package vn.edu.tdc.project_ltdd2_nhom5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddTransactionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_transaction);
+    }
+}
